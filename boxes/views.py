be42a2138fcebe094f0066317ca00773c0496477
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-06 10:59:47 +0100

    Add paths and view content to the boxes apps. was not added due to bad internet

--- boxes/views.py
+++ boxes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def boxes_view(request):
+    return render(request, 'boxes/boxes.html')  # make sure this template exists
