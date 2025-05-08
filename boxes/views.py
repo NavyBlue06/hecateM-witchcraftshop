c43e072f1a7c5518c37a4534388a8c6441e5e095
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-08 11:38:48 +0100

    Cleaned repo: added .gitignore and removed cached files

--- boxes/views.py
+++ boxes/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 
-def boxes_view(request):
+def all_products(request):
     return render(request, 'boxes/all_products.html')  # make sure this template exists
